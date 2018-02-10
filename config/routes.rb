810e5f4f095d96c24dae0c1a30a1b2fefe0625e2
Haruka Y <haruka.n.1122@gmail.com>
2018-02-10 23:42:02 +0000

    Finish User Destroy Action

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ SampleApp::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  resources :users, only: [:show]
-  root 'static_pages#home'
+  resources :users, only: [:show, :index, :destroy]
+  root  'static_pages#home'
+
   
   get "users/show"
   
